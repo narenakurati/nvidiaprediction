ab0cae8d53565b7ca40a6e5e919e3de99e6b9861
narenakurati <naren.akurati@gmail.com>
2018-07-12 22:23:44 +0200

    should be done

--- finalplot.R
+++ finalplot.R
@@ -17,9 +17,9 @@ ggsave(filename = "test.svg", plot=b)
 
 df1 <- data.frame("card" = c("1080", "1180"), "transistor density" = c(14793507294, 20329985870), "TFLOPs" = c(8873.00, 12328.65), "1080p performance" = c(127, 177.4223), "1600p performance" = c(161, 226.2672))
 
-d1 <- ggplot(df1, aes(x = card, y = transistor.density)) + geom_bar(stat = "identity", fill = "#2B83BA") + guides(fill=FALSE) + labs(y = "transistor density")
-d2 <- ggplot(df1, aes(x = card, y = TFLOPs)) + geom_bar(stat = "identity", fill = "#D7191C")  + guides(fill=FALSE)
-d3 <- ggplot(df1, aes(x = card, y = X1080p.performance)) + geom_bar(stat = "identity", fill = "#FDAE61")  + guides(fill=FALSE) + labs(y = "gaming performance")
+d1 <- ggplot(df1, aes(x = card, y = transistor.density)) + geom_bar(stat = "identity", fill = c("#203300", "#76b900")) + guides(fill=FALSE) + labs(y = "transistor density")
+d2 <- ggplot(df1, aes(x = card, y = TFLOPs)) + geom_bar(stat = "identity", fill = c("#203300", "#76b900"))  + guides(fill=FALSE)
+d3 <- ggplot(df1, aes(x = card, y = X1080p.performance)) + geom_bar(stat = "identity", fill = c("#203300", "#76b900"))  + guides(fill=FALSE) + labs(y = "gaming performance")
 
 finalgraphscomposite <- grid.arrange(d1,d2,d3, ncol =3, nrow =1)
 ggsave(filename = "finalgraphscomposite.svg", plot=finalgraphscomposite)
